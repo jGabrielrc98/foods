e0342b036159a4403fc7a49a014ecb6f4897217c
jGabrielrc98 <jgabrielrc7@gmail.com>
2025-04-09 14:01:35 -0300

    Estilização e Rotas

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: Colors.pink,
+          onPrimary: const Color.fromARGB(255, 255, 248, 224),
+          secondary: Colors.amber,
+          onSecondary: Colors.black,
+          error: Colors.red,
+          onError: Colors.white,
+          surface: Colors.white,
+          onSurface: Colors.white,
+        ),
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleMedium: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed'),
+        ),
+      ),
       home: CategoriesScreen(),
     );
   }
